2283561f973df38eaaddae805bd098d834d5d314
Viktor Åhlund <viktorahlund@Viktors-MacBook-Pro.local>
2019-06-03 16:25:56 +0200

    routing things

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -2,6 +2,8 @@ import { Mongo } from 'meteor/mongo';
  
 export const Tasks = new Mongo.Collection('tasks');
 
+Router.route('/');
+
 if (Meteor.isServer) {
     Meteor.publish('tasks', function subsPublication() {
         return Tasks.find();
@@ -9,7 +11,6 @@ if (Meteor.isServer) {
 
     Router.route('/get', {where: 'server'})
     .get(function(){
-        console.log("/GET HEYOOOO");
         var data = this.params.query.text;
         var res = Tasks.find({text: data}).fetch()
         this.response.statusCode = 200;
@@ -20,10 +21,13 @@ if (Meteor.isServer) {
     })
     Router.route('/post', {where: 'server'})
     .post(function(){
-        var params = this.params.query; // { _id: "5" }
-        var id = params.text; // "5"
-        var res = Tasks.insert({ text: id,
-        createdAt: new Date()})
+        console.log(this.request.body);
+
+        Tasks.insert({
+            text: this.request.body.text,
+            createdAt: this.request.body.createdAt,
+            test: this.request.body.test,
+        })
 
         this.response.statusCode = 200;
         this.response.setHeader("Access-Control-Allow-Origin", "http://localhost:3004");
@@ -33,10 +37,21 @@ if (Meteor.isServer) {
     })
     Router.route('/put', {where: 'server'})
     .put(function(){
+
         console.log("/PUT HEYOOOO");
         console.log("/POST HEYOOOO");
-        // var data = this.params.query.text;
-        // var res = Tasks.find({text: data}).fetch()
+        var data = this.params.query.text;
+        console.log("PUT: ", data);
+
+        try {
+            Tasks.find({text: data}).forEach(function(singleDoc) {
+                var searchId = singleDoc._id
+                console.log("ID of the document: ", searchId);
+                Tasks.update({_id: searchId}, { $set: { createdAt: new Date() } })
+            })  
+        } catch(e) {
+                console.log("error: ", e);
+            }
         this.response.statusCode = 200;
         this.response.setHeader("Access-Control-Allow-Origin", "http://localhost:3004");
         this.response.setHeader("Access-Control-Allow-Credentials", "true");
@@ -45,10 +60,16 @@ if (Meteor.isServer) {
     })
     Router.route('/delete', {where: 'server'})
     .delete(function(){
-        console.log("/DELETE HEYOOOO");
-        console.log("/POST HEYOOOO");
-        // var data = this.params.query.text;
-        // var res = Tasks.find({text: data}).fetch()
+        var data = this.params.query.text;
+        try {
+            Tasks.find({text: data}).forEach(function(singleDoc) {
+                var searchId = singleDoc._id
+                console.log("ID of the document: ", searchId);
+                Tasks.remove({_id: searchId})
+            })  
+        } catch(e) {
+                console.log("error: ", e);
+            }
         this.response.statusCode = 200;
         this.response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         this.response.setHeader("Access-Control-Allow-Credentials", "true");
